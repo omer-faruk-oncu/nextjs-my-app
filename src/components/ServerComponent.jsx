dc7f74a760e03032fbde1538009d1409470e095d
Omer <farukoncu78@gmail.com>
2024-05-29 23:07:11 +0300

    p

---
+++ src/components/ServerComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServerComponent = () => {
+    console.log("this is server component")
+  return (
+    <div>
+      ServerComponent
+    </div>
+  )
+}
+
+export default ServerComponent
