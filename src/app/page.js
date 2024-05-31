0c089d104f94b11e7f889f21a2581b17c51ff069
Omer <farukoncu78@gmail.com>
2024-05-31 12:06:41 +0300

    p

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 //? app folder içindeki page.js "/" route olan home sayfasıdır
 
+import ClientComponent from "@/components/ClientComponent";
+import ServerComponent from "@/components/ServerComponent";
+
 //? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use client" direktifini kullanmamız gerekir
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl">Hello from Home Page</h1>
+      <ServerComponent />
+      <ClientComponent />
     </main>
   );
 }
---
+++ src/components/ClientComponent.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+const ClientComponent = () => {
+    console.log("This is client component")
+  return (
+    <div>
+      ClientComponent
+    </div>
+  )
+}
+
+export default ClientComponent
